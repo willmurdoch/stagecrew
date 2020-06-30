37e214151ae80936ef0de5c2834446b2f02aa5f3
Will Murdoch <willmurdoch@gmail.com>
2020-06-30 17:10:28 -0400

    code output to copy into scene

--- js/stagecrew.js
+++ js/stagecrew.js
@@ -4,9 +4,21 @@ function stagecrew(camera, scene){
 
   var displayPanel = document.createElement('div');
   displayPanel.id = 'stagecrew';
-  displayPanel.innerHTML = '<div class="cameracrew"></div><div class="lightcrew"></div>';
+  displayPanel.innerHTML = '<div id="stagecrewCode"><div class="cameracrew"></div><div class="lightcrew"></div></div><button id="stagecrewCopy">COPY</button>';
   document.body.appendChild(displayPanel);
 
+  //Copy button
+  document.getElementById('stagecrewCopy').addEventListener('click', function(){
+    if (window.getSelection) {
+      var range = document.createRange();
+      range.selectNode(document.getElementById('stagecrewCode'));
+      window.getSelection().addRange(range);
+      document.execCommand("copy");
+      alert("Text has been copied, now paste in the text-area");
+    }
+  });
+
+  //Camera controls
   function cameracrew(){
     //Create controls for camera
     var cameraFolder = gui.addFolder('Camera');
@@ -23,21 +35,19 @@ function stagecrew(camera, scene){
   }
   cameracrew();
 
-  //Keep count of lights so they can get unique labels
-  var lightCount = 0;
+  //Light controls
   function lightcrew(type){
-    lightCount++;
 
     //Add new light to scene
     var light = new THREE.SpotLight(0xffffff, 1);
-    var lightInit = 'new THREE.SpotLight(0xffffff, 1);';
+    var lightInit = 'new THREE.SpotLight(0xffffff, 1)';
     if(type == 'AmbientLight'){
       light = new THREE.AmbientLight(0xffffff, 1);
-      lightInit = 'new THREE.AmbientLight(0xffffff, 1);';
+      lightInit = 'new THREE.AmbientLight(0xffffff, 1)';
     }
     if(type == 'PointLight'){
       light = new THREE.PointLight(0xffffff, 1);
-      lightInit = 'new THREE.PointLight(0xffffff, 1);';
+      lightInit = 'new THREE.PointLight(0xffffff, 1)';
     }
     if(type == 'RectLight'){
       light = new THREE.RectAreaLight(0xffffff, 1, 10, 10);
@@ -45,26 +55,51 @@ function stagecrew(camera, scene){
     }
     if(type == 'DirectionalLight'){
       light = new THREE.DirectionalLight(0xffffff, 1);
-      lightInit = 'new THREE.DirectionalLight(0xffffff, 1);';
+      lightInit = 'new THREE.DirectionalLight(0xffffff, 1)';
     }
     //Initial position so you can see something happened
   	light.position.set(-2, 28, 35);
   	scene.add(light);
 
+    var myProps = [];
     var codeOutput = document.createElement('div');
-    codeOutput.id = type + lightCount;
-    codeOutput.innerHTML = 'var '+ type + lightCount + ' = ' + lightInit + '<span class="properties"></span>';
+    codeOutput.id = type + light.id;
+    codeOutput.innerHTML = '<div class="light">var '+ type + light.id + ' = ' + lightInit + ';' + '</div><div class="properties"></div>';
     document.querySelector('#stagecrew .lightcrew').appendChild(codeOutput);
 
     //Editable properties for light
-    var lightFolder = gui.addFolder(type + ' ' + lightCount);
+    var lightFolder = gui.addFolder(type + ' ' + light.id);
     lightFolder.add(light.position, 'x', -200,200).listen().onChange(function(val){
-      document.querySelector('#' + type + lightCount + ' .properties').innerHTML = val;
+      myProps[type + light.id + '.position.x'] = val.toFixed(3);
     });
-    lightFolder.add(light.position, 'y', -200,200).listen();
-    lightFolder.add(light.position, 'z', -200,200).listen();
-    lightFolder.addColor(light, 'color').listen();
-    lightFolder.add(light, 'intensity', 0.01, 2.55).listen();
+    lightFolder.add(light.position, 'y', -200,200).listen().onChange(function(val){
+      myProps[type + light.id + '.position.y'] = val.toFixed(3);
+    });
+    lightFolder.add(light.position, 'z', -200,200).listen().onChange(function(val){
+      myProps[type + light.id + '.position.z'] = val.toFixed(3);
+    });
+    lightFolder.addColor(light, 'color').listen().onChange(function(val){
+      myProps[type + light.id + '.color'] = val;
+    });
+    lightFolder.add(light, 'intensity', 0.01, 2.55).listen().onChange(function(val){
+      myProps[type + light.id + '.intensity'] = val.toFixed(3);
+    });
+
+    //Loop to regenerate code for changed properties
+    setInterval(function(){
+      for (var key in myProps){
+        if (myProps.hasOwnProperty(key)){
+          var lightCode = key + ' = ' + myProps[key] + ';';
+          var lightID = key.split('.')[0];
+          if(!document.querySelector('#stagecrew #' + lightID + ' .properties [data-prop="' + key + '"]')){
+            var myProperty = document.createElement('div');
+            myProperty.dataset.prop = key;
+            document.querySelector('#stagecrew #' + lightID + ' .properties').appendChild(myProperty);
+          }
+          else document.querySelector('#stagecrew #' + lightID + ' .properties [data-prop="' + key + '"]').innerHTML = lightCode;
+        }
+      }
+    }, 1500);
 
     // light.position.x = 40;
     // light.position.y = 9;
